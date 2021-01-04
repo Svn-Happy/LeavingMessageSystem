8d6f5a2b549b531bbcb4f0f6bda8eae72c4feb51
Svn-Happy <2992515675@qq.com>
2021-01-04 14:31:35 +0800

    DBUtil.test

--- src/main/java/model/UserDAO.java
+++ src/main/java/model/UserDAO.java
@@ -1,11 +1,14 @@
 package model;
 
 import pojo.User;
+import util.DBUtil;
+
+import java.sql.Connection;
 
 public class UserDAO {
     /*
         数据库名messagesystem
-        表名：user  属性： id 、 password test
+        表名：user  属性： id 、 password
         表名：message 属性：num(主键、序号) 、  id（外键） 、 message 、 date
      */
 
@@ -13,6 +16,8 @@ public class UserDAO {
     //查询用户名是否存在、读取用户留言内容（加载用户所有信息）
     public User FindUser(String id){
         User user = new User();
+        Connection conn = DBUtil.getConn();
+
         return user;
     }
 
